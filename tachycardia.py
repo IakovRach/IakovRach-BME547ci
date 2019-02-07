d9ac47030e66cc8e7bdb791bd6181bfa5d115cb3
IakovRach <iakov.rachinskiy@duke.edu>
2019-02-07 18:42:24 -0500

    pep8 fixed

--- tachycardia.py
+++ tachycardia.py
@@ -1,18 +1,17 @@
 def is_tachycardic(str_in):
-
     str_in = str_in.lower()
-
     word = -1
     word = str_in.find('tachycardic')
-
     if(word >= 0):
         return True
     else:
         return False
 
+
 def user_input():
     str_in = input('Enter the word: ')
     return str_in
+
 
 if __name__ == "__main__":
     str_in = user_input()
