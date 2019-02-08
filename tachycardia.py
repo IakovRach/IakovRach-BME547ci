6d4bec8ec694e4a93d33b2ca6939dfbf1712a541
IakovRach <iakov.rachinskiy@duke.edu>
2019-02-07 22:28:07 -0500

    committing tachycardia.py module with function

---
+++ tachycardia.py
@@ -0,0 +1,24 @@
+def is_tachycardic(str_in):
+    str_in = str_in.lower()
+    word = -1
+    word = str_in.find('tachycardic')
+    if(word >= 0):
+        return True
+    else:
+        word = -1
+        word = str_in.find('tach')
+        if(word >= 0):
+            if
+
+
+def user_input():
+    str_in = input('Enter the word: ')
+    return str_in
+
+
+if __name__ == "__main__":
+    str_in = user_input()
+    if(is_tachycardic(str_in)):
+        print('True!')
+    else:
+        print('False!')
